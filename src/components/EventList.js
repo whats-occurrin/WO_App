ad46721bc40d4dfaae5bb94ac5f90c7ce6b9c269
Gemma <gemmawvoakes@gmail.com>
2017-07-19 16:56:26 +0100

    Added [Map], [EventList] and [Event] components

---
+++ src/components/EventList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import Event from './Event';
+
+const EventList = () => {
+    return (
+        <Event />
+    );
+};
+
+export default EventList;
